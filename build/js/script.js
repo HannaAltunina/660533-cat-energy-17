5c8b6a111a448a9f0c5c185ef93ab7ee10051963
Hanna Altunina <hanna.aljaxeeva@gmail.com>
2019-08-29 20:51:42 +0300

    правки сеток и js

--- build/js/script.js
+++ build/js/script.js
@@ -1,19 +1,19 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle--opened');
+var navToggle = document.querySelector('.header__nav-toggle--opened');
 
-navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('header__nav-toggle--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navToggle.classList.contains('main-nav__toggle--opened')) {
+  if (navToggle.classList.contains('header__nav-toggle--opened')) {
     navMain.classList.remove('main-nav');
     navMain.classList.add('main-nav--closed');
-    navToggle.classList.remove('main-nav__toggle--opened');
-    navToggle.classList.add('main-nav__toggle--closed');
+    navToggle.classList.remove('header__nav-toggle--opened');
+    navToggle.classList.add('header__nav-toggle--closed');
   } else {
     navMain.classList.add('main-nav');
     navMain.classList.remove('main-nav--closed');
-    navToggle.classList.remove('main-nav__toggle--closed');
-    navToggle.classList.add('main-nav__toggle--opened');
+    navToggle.classList.remove('header__nav-toggle--closed');
+    navToggle.classList.add('header__nav-toggle--opened');
   }
 });
 
@@ -38,24 +38,3 @@ btnSubmit.addEventListener('click', function (evt) {
     }
   }
 });
-
-/*
-function initMap() {
-var coordinates = {lat: 59.938663, lng: 30.323016},
-  map = new google.maps.Map(document.getElementById('footer__map'), {
-      center: coordinates
-  });
-
-  marker = new google.maps.Marker({
-      position: coordinates,
-      map: map
-  });
-
-  image = 'img/map-pin.png',
-  marker = new google.maps.Marker({
-    position: coordinates,
-    map: map,
-    icon: image
-  });
-}
-*/
